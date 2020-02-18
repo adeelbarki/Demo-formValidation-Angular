d5c9a13283adfa6d71532ad5e48389167ed99283
Adeel Ahmed Khan <adeelbarki@gmail.com>
2020-02-18 14:36:13 +0500

    added validation to firstname and last name

--- src/app/user/create-user.component.ts
+++ src/app/user/create-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent implements OnInit {
+  userForm: FormGroup;
 
-  constructor() { }
+  ValidationMessages = {
+    firstName: {
+      required: 'First Name is required',
+      minlength: 'Must be 2 characters',
+      maxlength: 'Must be less than 10 characters'
+    },
+    lastName: {
+      required: 'First Name is required',
+      minlength: 'Must be 2 characters',
+      maxlength: 'Must be less than 10 characters'
+    }
+  };
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.userForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+    });
   }
-
 }
